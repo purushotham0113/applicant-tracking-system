93308add71c5fe86fb9129b54f34539281a13354
purushotham0113 <229x1a33b3@gmail.com>
2025-09-12 06:10:19 +0530

    added the proxy

--- server/server.js
+++ server/server.js
@@ -22,6 +22,7 @@ import connectDB from './config/database.js';
 dotenv.config();
 
 const app = express();
+app.set("trust proxy", 1);
 const PORT = process.env.PORT || 5500;
 
 // Connect to MongoDB
